559c871c924b413cc1e3adb9526c44f47a10c277
Emerson <edavid.manaog@gmail.com>
2025-04-28 21:49:15 +1000

    Create ES10.js

---
+++ Advanced Javascript/ES10.js
@@ -0,0 +1,28 @@
+// const array = [1, 2, 3, 4, 5]
+
+// array.flat()
+
+// array.flatMap(number => {
+//     return number * 2
+// })
+
+
+// userEmail = '       eddytheeagle@gmail.com'
+
+// userEmail.trimStart()
+
+// userEmail.trimEnd()
+
+userProfile = [['commanderTom', 23], ['derekZlander', 40], ['hansel', 18]]
+
+Object.fromEntries(userProfile)
+
+try {
+    true + 'hi'
+} catch (error) {
+    console.log('you messed up' + error)
+} finally {
+    console.log('finally')
+}
+
+
